a0ef6ce3182f3f3428213b7397b950f6411d6975
8ball030 <tomrae030@hotmail.com>
2020-10-31 23:24:18 +0000

    updated readme

--- frontend/src/pages/bot_management/AgentList.js
+++ frontend/src/pages/bot_management/AgentList.js
@@ -24,11 +24,7 @@ function createData(name, calories, fat, carbs, protein, status) {
 }
 
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0, 'paused'),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3, 'error'),
-  createData('Eclair', 262, 16.0, 24, 6.0, 'running'),
-  createData('Cupcake', 305, 3.7, 67, 4.3, 'running'),
-  createData('Gingerbread', 356, 16.0, 49, 3.9, 'running'),
+  createData('', '', '', '', '', '', ''),
 ];
 const states = {
   running: {
@@ -46,6 +42,7 @@ const states = {
 };
 
 class AgentList extends React.Component {
+  state = {agentsData: rows};
 
   constructor (props) {
     super(props);
@@ -57,6 +54,7 @@ class AgentList extends React.Component {
     API.get('get_all_agents')
       .then(agents => agents.data)
       .then(agentsList => {
+          console.log(agentsList)
           this.setState({ agentsData: agentsList });
       });
   }
@@ -67,6 +65,8 @@ class AgentList extends React.Component {
 
   render() {
 
+    const {agentsData} = this.state;
+
 
     return (
         <TableContainer component={Paper}>
@@ -76,22 +76,22 @@ class AgentList extends React.Component {
                 <TableCell>Agent Public Address</TableCell>
                 <TableCell align="right">Date Created</TableCell>
                 <TableCell align="right">Last Updated</TableCell>
-                <TableCell align="right">Status</TableCell>
                 <TableCell align="right">USD Value</TableCell>
                 <TableCell align="right">ETH Value</TableCell>
+                <TableCell align="right">Status</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
-              {rows.map((row) => (
+              {agentsData.map((row) => (
                 <TableRow key={row.address}>
                   <TableCell component="th" scope="row">
-                    {row.name}
+                    {row.address}
                   </TableCell>
                   <TableCell align="right">{row.date_created}</TableCell>
                   <TableCell align="right">{row.date_updated}</TableCell>
                   <TableCell align="right">{row.usd_val}</TableCell>
-                  <TableCell align="right">
-                    <Button
+                  <TableCell align="right">{row.eth_val}</TableCell>  
+                  <TableCell align="right">  <Button
                       style={states[row.status]}
                     >
                       {row.status}
